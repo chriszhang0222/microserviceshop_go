19013c3e28c1067c390f0b514f77efe0c793f7bd
chriszhang94 <chris@quantumsds.com>
2020-12-11 15:45:07 -0500

    log file

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
-module "mxshop"
+module mxshop
+
+go 1.14
+
+require (
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0
+)
--- user_web/main.go
+++ user_web/main.go
@@ -1,3 +1,24 @@
 package main
 
+import (
+	"go.uber.org/zap"
+)
+
+func NewLogger()(*zap.Logger, error){
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{
+		"./myproject.log",
+		"stderr",
+	}
+	return cfg.Build()
+}
+func main(){
+	logger, err := NewLogger()
+	if err != nil{
+		panic(err)
+	}
+	defer logger.Sync()
+	logger.Info("msg...")
+
+}
 
