ec9d29a70f4b27425efd16a58291915a8cd8b680
chriszhang94 <chris@quantumsds.com>
2020-12-17 17:06:04 -0500

    health check

--- user_web/forms/userform.go
+++ user_web/forms/userform.go
@@ -2,7 +2,7 @@ package forms
 
 type PasswordLoginForm struct{
 	Mobile string `form:"mobile" json:"mobile" binding:"required,mobile"`
-	Password string `form:"password" json:"password" binding:"required,min=3,max=20"`
+	Password string `form:"password" json:"password" binding:"required,min=6,max=20"`
 	Captcha string `form:"captcha" json:"captcha" binding:"required"`
 	CaptchaId string `form:"captcha_id" json:"captcha_id" binding:"required"`
 }
@@ -10,5 +10,5 @@ type PasswordLoginForm struct{
 type RegisterForm struct {
 	Mobile string `form:"mobile" json:"mobile" binding:"required,mobile"`
 	Nickname string `form:"nickname" json:"nickname" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required,min=6,max=20"`
 }
--- user_web/initialize/routerinit.go
+++ user_web/initialize/routerinit.go
@@ -3,11 +3,18 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 	"mxshop/user_web/middleware"
+	"net/http"
 )
 import r "mxshop/user_web/router"
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+	Router.GET("/health", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"code": http.StatusOK,
+			"success": true,
+		})
+	})
 	Router.Use(middleware.Cors())
 	ApiGroup := Router.Group("/v1")
 	r.InitUserRouter(ApiGroup)
