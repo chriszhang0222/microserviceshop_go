2a29873242d19e568f056f37f1378d9088071836
chriszhang94 <chris@quantumsds.com>
2021-01-05 18:44:38 -0500

    new order

--- order_web/api/order.go
+++ order_web/api/order.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"mxshop/order_web/forms"
 	"mxshop/order_web/global"
 	"mxshop/order_web/proto"
 	"net/http"
@@ -51,5 +52,72 @@ func OrderList(ctx *gin.Context){
 	}
 	reMap["data"] = orderList
 	ctx.JSON(http.StatusOK, reMap)
+
+}
+
+func NewOrder(ctx *gin.Context){
+	orderForm := forms.CreateOrderForm{}
+	if err := ctx.ShouldBindJSON(&orderForm); err != nil {
+		HandleValidatorError(ctx, err)
+	}
+	userId, _ := ctx.Get("userId")
+	rsp, err := global.OrderSrvClient.CreateOrder(context.Background(), &proto.OrderRequest{
+		UserId: int32(userId.(uint)),
+		Name: orderForm.Name,
+		Mobile: orderForm.Mobile,
+		Address: orderForm.Address,
+		Post: orderForm.Post,
+	})
+	if err != nil {
+		zap.S().Errorw("新建订单失败")
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"id": rsp.Id,
+	})
+}
+
+func OrderDetail(ctx *gin.Context){
+	id := ctx.Param("id")
+	userId, _ := ctx.Get("userId")
+	i, _ := strconv.Atoi(id)
+	request := proto.OrderRequest{
+		Id: int32(i),
+		UserId: int32(userId.(uint)),
+	}
+	rsp, err := global.OrderSrvClient.OrderDetail(context.Background(), &request)
+	if err != nil {
+		zap.S().Errorw("获取订单详情失败")
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	reMap := gin.H{}
+	reMap["id"] = rsp.OrderInfo.Id
+	reMap["status"] = rsp.OrderInfo.Status
+	reMap["user"] = rsp.OrderInfo.UserId
+	reMap["post"] = rsp.OrderInfo.Post
+	reMap["total"] = rsp.OrderInfo.Total
+	reMap["address"] = rsp.OrderInfo.Address
+	reMap["name"] = rsp.OrderInfo.Name
+	reMap["mobile"] = rsp.OrderInfo.Mobile
+	reMap["pay_type"] = rsp.OrderInfo.PayType
+	reMap["order_sn"] = rsp.OrderInfo.OrderSn
+
+	goodsList := make([]interface{}, 0)
+	for _, item := range rsp.Data {
+		tmpMap := gin.H{
+			"id": item.GoodsId,
+			"name": item.GoodsName,
+			"image": item.GoodsImage,
+			"price": item.GoodsPrice,
+			"nums": item.Nums,
+		}
+
+		goodsList = append(goodsList, tmpMap)
+	}
+	reMap["goods"] = goodsList
+	ctx.JSON(http.StatusOK, reMap)
+
 
 }
--- order_web/routers/order.go
+++ order_web/routers/order.go
@@ -10,5 +10,7 @@ func InitOrderRouter(Router *gin.RouterGroup){
 	OrderRouter := Router.Group("order").Use(middleware.JWTAUth())
 	{
 		OrderRouter.GET("", api.OrderList)
+		OrderRouter.GET("/:id", api.OrderDetail)
+		OrderRouter.POST("", api.NewOrder)
 	}
 }
