bb7f5c3f6ca194d2b098a24210a15d0c8a84fbb6
chriszhang94 <chris@quantumsds.com>
2021-01-06 11:15:21 -0500

    goroutine to register

--- order_web/initialize/srv_conn.go
+++ order_web/initialize/srv_conn.go
@@ -2,8 +2,8 @@ package initialize
 
 import (
 	"fmt"
-	"go.uber.org/zap"
 	_ "github.com/mbobakov/grpc-consul-resolver"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"mxshop/order_web/global"
 	"mxshop/order_web/proto"
@@ -11,49 +11,8 @@ import (
 )
 
 var wg sync.WaitGroup
-func InitSrvConn(){
-	consulInfo := global.ServerConfig.ConsulInfo
-		orderConn, err := grpc.Dial(
-			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.OrderSrvInfo.Name),
-			grpc.WithInsecure(),
-			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`))
-
-		if err != nil{
-			zap.S().Fatalf("[InitSrvConn] Failed to connect to %s", global.ServerConfig.OrderSrvInfo.Name)
-			return
-		}
-		global.OrderSrvClient = proto.NewOrderClient(orderConn)
-
-
-
-		goodsConn, err := grpc.Dial(
-			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.GoodsSrvInfo.Name),
-			grpc.WithInsecure(),
-			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
-		)
-		if err != nil {
-			zap.S().Fatal("[InitSrvConn]  Failed to connect to GoodsService")
-		}
-
-		global.GoodsSrvClient = proto.NewGoodsClient(goodsConn)
-
-
-		invConn, err := grpc.Dial(
-			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.InventoryInfo.Name),
-			grpc.WithInsecure(),
-			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
-		)
-		if err != nil {
-			zap.S().Fatal("[InitSrvConn] Failed to connect to Inventory service")
-		}
-
-		global.InventorySrvClient = proto.NewInventoryClient(invConn)
-}
 //func InitSrvConn(){
 //	consulInfo := global.ServerConfig.ConsulInfo
-//	wg.Add(3)
-//	go func() {
-//		defer wg.Done()
 //		orderConn, err := grpc.Dial(
 //			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.OrderSrvInfo.Name),
 //			grpc.WithInsecure(),
@@ -64,10 +23,9 @@ func InitSrvConn(){
 //			return
 //		}
 //		global.OrderSrvClient = proto.NewOrderClient(orderConn)
-//	}()
 //
-//	go func() {
-//		defer wg.Done()
+//
+//
 //		goodsConn, err := grpc.Dial(
 //			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.GoodsSrvInfo.Name),
 //			grpc.WithInsecure(),
@@ -78,10 +36,8 @@ func InitSrvConn(){
 //		}
 //
 //		global.GoodsSrvClient = proto.NewGoodsClient(goodsConn)
-//	}()
 //
-//	go func() {
-//		defer wg.Done()
+//
 //		invConn, err := grpc.Dial(
 //			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.InventoryInfo.Name),
 //			grpc.WithInsecure(),
@@ -92,6 +48,50 @@ func InitSrvConn(){
 //		}
 //
 //		global.InventorySrvClient = proto.NewInventoryClient(invConn)
-//	}()
-//	wg.Wait()
 //}
+func InitSrvConn(){
+	consulInfo := global.ServerConfig.ConsulInfo
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		orderConn, err := grpc.Dial(
+			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.OrderSrvInfo.Name),
+			grpc.WithInsecure(),
+			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`))
+
+		if err != nil{
+			zap.S().Fatalf("[InitSrvConn] Failed to connect to %s", global.ServerConfig.OrderSrvInfo.Name)
+			return
+		}
+		global.OrderSrvClient = proto.NewOrderClient(orderConn)
+	}()
+
+	go func() {
+		defer wg.Done()
+		goodsConn, err := grpc.Dial(
+			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.GoodsSrvInfo.Name),
+			grpc.WithInsecure(),
+			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+		)
+		if err != nil {
+			zap.S().Fatal("[InitSrvConn]  Failed to connect to GoodsService")
+		}
+
+		global.GoodsSrvClient = proto.NewGoodsClient(goodsConn)
+	}()
+
+	go func() {
+		defer wg.Done()
+		invConn, err := grpc.Dial(
+			fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.InventoryInfo.Name),
+			grpc.WithInsecure(),
+			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+		)
+		if err != nil {
+			zap.S().Fatal("[InitSrvConn] Failed to connect to Inventory service")
+		}
+
+		global.InventorySrvClient = proto.NewInventoryClient(invConn)
+	}()
+	wg.Wait()
+}
