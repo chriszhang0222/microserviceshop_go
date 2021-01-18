65772ceae5614c37d10306176b4f2b666e7e93e7
chriszhang94 <chris@quantumsds.com>
2021-01-17 20:31:09 -0500

    consumer

--- common/rabbitMQ/config.go
+++ common/rabbitMQ/config.go
@@ -1,6 +1,8 @@
 package rabbitMQ
 import (
+	"fmt"
 	"github.com/streadway/amqp"
+	"go.uber.org/zap"
 )
 type Rabbitconfig struct{
 	Host string
@@ -13,3 +15,21 @@ type Rabbitconfig struct{
 var Config = &Rabbitconfig{}
 var conn *amqp.Connection
 var channel *amqp.Channel
+
+
+func InitChannel()bool{
+	if channel != nil {
+		return true
+	}
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://guest:guest@%s:%d/", Config.Host, Config.Port))
+	if err != nil {
+		zap.S().Error(err)
+		return false
+	}
+	channel, err = conn.Channel()
+	if err != nil {
+		zap.S().Error(err)
+		return false
+	}
+	return true
+}
--- common/rabbitMQ/producer.go
+++ common/rabbitMQ/producer.go
@@ -1,1 +1,17 @@
 package rabbitMQ
+
+import "github.com/streadway/amqp"
+
+func Publish(exchange, routingKey string, msg []byte)bool{
+	if nil == channel.Publish(
+		exchange,
+		routingKey,
+		false, // 如果没有对应的queue, 就会丢弃这条消息
+		false, //
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        msg}) {
+		return true
+	}
+	return false
+}
