31b3c93a0fd0aaf524c6442e093bea88f05f0b68
chriszhang94 <chris@quantumsds.com>
2021-02-17 20:19:36 -0500

    jaeger tracing

--- goods_web/initialize/srv_conn.go
+++ goods_web/initialize/srv_conn.go
@@ -3,10 +3,12 @@ package initialize
 import (
 	"fmt"
 	_ "github.com/mbobakov/grpc-consul-resolver"
+	"github.com/opentracing/opentracing-go"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"mxshop/goods_web/global"
 	"mxshop/goods_web/proto"
+	"mxshop/common/otgrpc"
 )
 
 func InitSrvConn(){
@@ -14,7 +16,8 @@ func InitSrvConn(){
 	goodsConn, err := grpc.Dial(
 		fmt.Sprintf("consul://%s:%d/%s?wait=14s", consulInfo.Host, consulInfo.Port, global.ServerConfig.GoodsSrvInfo.Name),
 		grpc.WithInsecure(),
-		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`), )
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+		grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer())))   //jaeger tracing
 	if err != nil{
 		zap.S().Fatalf("[InitSrvConn] Failed to connect to %s", global.ServerConfig.GoodsSrvInfo.Name)
 		return
